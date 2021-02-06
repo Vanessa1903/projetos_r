8bca790a132a6b855d6fe105fbad26b65525fc84
Vanessa1903 <vanessastuart1903@gmail.com>
2021-02-06 10:45:53 -0300

    06/02

--- Linear_regression.R
+++ Linear_regression.R
@@ -51,12 +51,65 @@ galton_heights %>%
   ggplot(aes(father, son)) +
   geom_point(alpha = 0.5)
 
+# correlação: duas formas
+rho <- mean(scale(x)*scale(y))
+galton_heights %>% summarize(r = cor(father, son)) %>% pull(r)
+##ou cor(galton_heights$father, galton_heights$son)
+
+####selecionando amostra
+# compute sample correlation
+R <- sample_n(galton_heights, 25, replace = TRUE) %>%
+  summarize(r = cor(father, son))
+R
+
+# Monte Carlo simulation to show distribution of sample correlation
+B <- 1000
+N <- 25
+R <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    summarize(r = cor(father, son)) %>%
+    pull(r)
+})
+qplot(R, geom = "histogram", binwidth = 0.05, color = I("black"))
+
+# expected value and standard error
+mean(R)
+sd(R)
+
+# QQ-plot to evaluate whether N is large enough
+data.frame(R) %>%
+  ggplot(aes(sample = R)) +
+  stat_qq() +
+  geom_abline(intercept = mean(R), slope = sqrt((1-mean(R)^2)/(N-2)))
 
 
+##questions
 
+#What is the correlation coefficient between number of runs per game and 
+#number of at bats per game?
+Teams_small <- Teams %>% filter(yearID %in% 1961:2001)
+cor(Teams_small$R/Teams_small$G, Teams_small$AB/Teams_small$G)
 
+#What is the correlation coefficient between win rate (number of wins per game) 
+#and number of errors per game?
+cor(Teams_small$W/Teams_small$G, Teams_small$E/Teams_small$G)
 
+#What is the correlation coefficient between doubles (X2B) per game and triples 
+#(X3B) per game?
+cor(Teams_small$X2B/Teams_small$G, Teams_small$X3B/Teams_small$G)
 
+# compute a regression line to predict the son's height from the father's height
+mu_x <- mean(galton_heights$father)
+mu_y <- mean(galton_heights$son)
+s_x <- sd(galton_heights$father)
+s_y <- sd(galton_heights$son)
+r <- cor(galton_heights$father, galton_heights$son)
+m_1 <-  r * s_y / s_x
+b_1 <- mu_y - m_1*mu_x
+
+# compute a regression line to predict the father's height from the son's height
+m_2 <-  r * s_x / s_y
+b_2 <- mu_x - m_2*mu_y
 
 
 
