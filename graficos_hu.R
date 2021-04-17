caae1e5aac4821db97fb6b7c02039231623cdbbc
Vanessa1903 <vanessastuart1903@gmail.com>
2021-04-17 15:08:51 -0300

    17/04

--- graficos_hu.R
+++ graficos_hu.R
@@ -21,15 +21,15 @@ names(questio)
 
 exames <- read.csv("~/projetos_r/exames"); head(exames)
 exames$X <- NULL
+names(exames)
 
 quant <- read.csv("~/projetos_r/quant"); head(quant)
-names(quant)
 quant$X <- NULL
+names(quant)
 
 quali <- read.csv("~/projetos_r/quali"); head(quali)
-names(quali)
 quali$X <- NULL
-
+names(quali)
 
 dim(bd) #ver dimensao do bd
 par(bd)
@@ -91,7 +91,7 @@ pairs(~ shaps + acips_total + teps_total + hads_total + neuroticismo
       pch = 19, lwd = 2, col = as.factor(bd$sexo), lower.panel = panel.smooth)
 
 
-###########TESTE 1 - MODELO LINEAR###########
+###########TESTE 1 - MODELO LINEAR#####################
 #####STEPWISE#####
 #X = shaps, acips, hads, segmentados, linfocitos, eosinofilos
 
@@ -198,96 +198,383 @@ plot(predict(modelo2), rstudent(modelo2), pch=19, ylim=c(-3,3)); abline(h=c(-3,0
 #limite de -3,3, fora disso é outlier
 par(mfrow=c(1,1))
 
-##############PROPOSTA 1: TEPS VARIAVEL DEPENDENTE################3
+##############PROPOSTA 1: TEPS VARIAVEL DEPENDENTE#######################
 
 ###INDEPENDENTES: sociodemograficas e exames
 
 ##sugestoes: idade, imc, hemograma, cortisol, crh, pcr
 names(bd)
 
-m1 <- lm(teps_total ~ idade + imc + cortisol + CRH + PCR + sexo +
-           hemacias + hemoglobina + hematocrito + volumeglob +
+m1 <- na.omit(lm(teps_total ~ idade + cortisol + CRH + PCR +
            hemoglobina_concent + rdw + leucocitos + segmentados +
            bastonados + linfocitos + monocitos + eosinofilos + 
-           basofilos + plaquetas + plaquetas_vol, bd)
+           basofilos + plaquetas_vol + anos_estudo + renda_familia +
+           tempo_tabagismo + exercicio_tempo +
+           exercicio_duracao + traumatismo_tempo + internacao_quant, bd))
 summary(m1)
 anova(m1)
 plot(m1)
 na.omit(step(m1))
 
+names(quant)
+
 ##mudou
-m2 <- lm(teps_total ~ idade + imc + cortisol + CRH + PCR + sexo + hemacias + 
-           hemoglobina + hematocrito + volumeglob + hemoglobina_concent + 
-           rdw + segmentados + bastonados + linfocitos + monocitos + 
-           plaquetas + plaquetas_vol, bd)
-summary(m2)
-na.omit(step(m2))
-anova(m2) #segmentados signif
+m2 <- lm(teps_total ~ idade + CRH + PCR + hemoglobina_concent + 
+           leucocitos + segmentados + linfocitos + monocitos + eosinofilos + 
+           plaquetas_vol + anos_estudo + renda_familia + exercicio_tempo + 
+           exercicio_duracao + internacao_quant, bd)
+bd$exercicio_duracao
+bd$exercicio_tempo
+bd$internacao_quant
+summary(m2) #R: 0,6854
+anova(m2) 
+##menos contribuem - valor-p
+#cor - renda (-0,0855), leucocitos (-0,1912), idade (-0,2164)
 AIC(m1, m2) #m2
 vif(m2)
+step(m2)
+##VIF>=10 perigoso: cor - segmentados (-0,2948), linfocitos (0,3159), monocitos (0,1955)
+#eosinofilos (-0,322)
+m_perigo <- lm(teps_total ~ segmentados + linfocitos + monocitos + eosinofilos, bd)
+summary(m_perigo)
+vif(m_perigo)
+step(m_perigo) #segmentados e eosinofilos
 
-m3 <- lm(teps_total ~ idade + imc + cortisol + CRH + PCR + sexo + hemacias + 
-           hemoglobina + volumeglob + hemoglobina_concent + 
-           rdw + segmentados + bastonados + linfocitos + monocitos + 
-           plaquetas + plaquetas_vol, bd)
-#tira hematocrito, hemacias, hemoglobina, volumeglob, hemog_concent
-#deixa volumeglob
-summary(m3)
-anova(m3)
-AIC(m2, m3) #m2
-
-m4 <- lm(teps_total ~ idade + imc + cortisol + CRH + PCR + sexo + volumeglob
-           + rdw + segmentados + bastonados + linfocitos + monocitos + 
-           plaquetas + plaquetas_vol, bd)
-summary(m4)
-anova(m4)
-AIC(m3, m4) #m4
-na.omit(step(m4))
-
-m5 <- lm(teps_total ~ idade + PCR + linfocitos + plaquetas + plaquetas_vol, bd)
-summary(m5)
-anova(m5)
-AIC(m5, m4) #m5
-na.omit(step(m5))
-vif(m5)
-#tira plaquetas (valor-p muito alto)
-
-m6 <- lm(teps_total ~ idade + PCR + linfocitos + plaquetas_vol, bd)
-summary(m6)
-anova(m6)
-AIC(m5, m6) #m5
-
-#tentando tirar a idade
-m7 <- lm(teps_total ~ PCR + linfocitos + plaquetas + plaquetas_vol, bd)
-summary(m7)
-anova(m7)
-AIC(m5, m7) #m5
-
-#############MELHORANDO M5#############################
-
-mod <- data.frame(bd$teps_total, bd$idade, bd$PCR, bd$linfocitos, bd$plaquetas,
-                  bd$plaquetas_vol)
-
-m <- na.omit(mod); head(m)
-round(cor(m),2)   #matriz de correlacao
-###correlacao de todos baixa com o teps
-##baixa correlacao entre eles
-vif(mod)
-##sem multicolinearidade
-m5 <- lm(teps_total ~ idade + PCR + linfocitos + plaquetas + plaquetas_vol, bd)
-summary(m5)
-anova(m5)
+##ACP: menos contribuem
+#PC19 = renda, idade, leucocitos (qualquer um q eu tire, o m2 continua sendo preferivel)
 
 ####teste de residuos
-
 #normalidade
-shapiro.test(rstudent(m5))    #-aceita p>0,05   #residuos
-qqPlot(rstudent(m5), pch=16)  #dentro do envelope   #residuos
+shapiro.test(rstudent(m2))    #-aceita p>0,05   #residuos
+qqPlot(rstudent(m2), pch=16)  #dentro do envelope   #residuos
 #variancia - homoscedasticidade
-plot(predict(m5), rstudent(m5), pch=19, ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2)
-bartlett.test(mod) 
-#na.omit(bartlett.test(teps_total ~ idade + PCR + linfocitos + plaquetas + plaquetas_vol, bd))
-#nao funcionou
+plot(predict(m2), rstudent(m2), pch=19, ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2)
+
+##sem linfocito e monocito
+m2_2 <- lm(teps_total ~ idade + CRH + PCR + hemoglobina_concent + 
+             leucocitos + segmentados + eosinofilos +
+             plaquetas_vol + anos_estudo + renda_familia + exercicio_tempo + 
+             exercicio_duracao + internacao_quant, bd)
+summary(m2_2) #R: 0,1541
+AIC(m2, m2_2) #m2, mas tem multic
+vif(m2_2)
+step(m2_2)
+####teste de residuos
+#normalidade
+shapiro.test(rstudent(m2_2))    #-aceita p>0,05   #residuos
+qqPlot(rstudent(m2_2), pch=16)  #dentro do envelope   #residuos
+#variancia - homoscedasticidade
+plot(predict(m2_2), rstudent(m2_2), pch=19, ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2)
+
+
+m3 <- lm(teps_total ~ idade + CRH + PCR + hemoglobina_concent + leucocitos + 
+           segmentados + eosinofilos + plaquetas_vol + anos_estudo + 
+           renda_familia + exercicio_tempo, bd)
+summary(m3) #0,2945
+vif(m3)
+AIC(m2, m3) #m2, mas o m3 nao tem multicolinearidade
+AIC(m3, m2_2) #m3
+step(m3)
+####teste de residuos
+#normalidade
+shapiro.test(rstudent(m3))    #-aceita p>0,05   #residuos
+qqPlot(rstudent(m3), pch=16)  #dois fora do envelope   #residuos
+#variancia - homoscedasticidade
+plot(predict(m3), rstudent(m3), pch=19, ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2)
+    #um fora
+
+##tirando idade
+m3_3 <- lm(teps_total ~ CRH + PCR + hemoglobina_concent + leucocitos + 
+           segmentados + eosinofilos + plaquetas_vol + anos_estudo + 
+           renda_familia + exercicio_tempo, bd)
+summary(m3_3) #0,3469
+vif(m3_3)
+AIC(m3, m3_3, m2) #m2, mas o m3 nao tem multicolinearidade 
+##ou m_3_3
+step(m3_3)
+####teste de residuos
+#normalidade
+shapiro.test(rstudent(m3_3))    #-aceita p>0,05   #residuos
+qqPlot(rstudent(m3_3), pch=16)  #dois fora do envelope   #residuos
+#variancia - homoscedasticidade
+plot(predict(m3_3), rstudent(m3_3), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok
+
+
+
+
+
+
+
+
+
+####modelo 2 - tentativas de melhorar
+
+m2 <- lm(teps_total ~ idade + CRH + PCR + hemoglobina_concent + 
+           leucocitos + segmentados + linfocitos + monocitos + eosinofilos + 
+           plaquetas_vol + anos_estudo + renda_familia + exercicio_tempo + 
+           exercicio_duracao + internacao_quant, bd)
+summary(m2) #R: 0,6854
+anova(m2) 
+##menos contribuem - valor-p
+#cor - renda (-0,0855), leucocitos (-0,1912), idade (-0,2164)
+
+AIC(m1, m2) #m2
+vif(m2)
+step(m2)
+##VIF>=10 perigoso: cor - segmentados (-0,2948), linfocitos (0,3159), monocitos (0,1955)
+#eosinofilos (-0,322)
+
+##ACP: menos contribuem
+#PC19 = renda, idade, leucocitos (qualquer um q eu tire, o m2 continua sendo preferivel)
+
+####teste de residuos
+#normalidade
+shapiro.test(rstudent(m2))    #-aceita p>0,05   #residuos
+qqPlot(rstudent(m2), pch=16)  #dentro do envelope   #residuos
+#variancia - homoscedasticidade
+plot(predict(m2), rstudent(m2), pch=19, ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2)
+
+m_idade <- lm(teps_total ~ idade, bd)
+summary(m_idade) #R: 0,02 p = 0,153
+shapiro.test(rstudent(m_idade))    #-aceita p>0,05
+qqPlot(rstudent(m_idade), pch=16)  #dentro do envelope 
+plot(predict(m_idade), rstudent(m_idade), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok
+
+m_crh <- lm(teps_total ~ CRH, bd)
+summary(m_crh) #R: -0,02 p = 0,888
+shapiro.test(rstudent(m_crh))    #-aceita p>0,05
+qqPlot(rstudent(m_crh), pch=16)  #dentro do envelope 
+plot(predict(m_crh), rstudent(m_crh), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #com tendencia
+
+m_pcr <- lm(teps_total ~ PCR, bd)
+summary(m_pcr) #R: 0,0002 p = 0,3237
+shapiro.test(rstudent(m_pcr))    #-aceita p>0,05
+qqPlot(rstudent(m_pcr), pch=16)  #dentro do envelope 
+plot(predict(m_pcr), rstudent(m_pcr), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #com tendencia
+
+m_hemo <- lm(teps_total ~ hemoglobina_concent, bd)
+summary(m_hemo) #R: -0,0237 p = 0,948
+shapiro.test(rstudent(m_hemo))    #-aceita p>0,05
+qqPlot(rstudent(m_hemo), pch=16)  #dentro do envelope 
+plot(predict(m_hemo), rstudent(m_hemo), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok
+
+m_leuc <- lm(teps_total ~ leucocitos, bd)
+summary(m_leuc) #R: 0,0141 p: 0,208
+shapiro.test(rstudent(m_leuc))    #-aceita p>0,05
+qqPlot(rstudent(m_leuc), pch=16)  #dentro do envelope 
+plot(predict(m_leuc), rstudent(m_leuc), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #tendencia
+
+m_seg <- lm(teps_total ~ segmentados, bd)
+summary(m_seg) #R: 0,0656 p: 0,0493
+shapiro.test(rstudent(m_seg))    #-aceita p>0,05
+qqPlot(rstudent(m_seg), pch=16)  #dentro do envelope 
+plot(predict(m_seg), rstudent(m_seg), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok?
+
+m_linf <- lm(teps_total ~ linfocitos, bd)
+summary(m_linf) #R: 0,0789 p: 0,0345
+shapiro.test(rstudent(m_linf))    #-aceita p>0,05
+qqPlot(rstudent(m_linf), pch=16)  #dentro do envelope 
+plot(predict(m_linf), rstudent(m_linf), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok?
+
+m_monoc <- lm(teps_total ~ monocitos, bd)
+summary(m_monoc) #R: 0,0158 p: 0,198
+shapiro.test(rstudent(m_monoc))    #-aceita p>0,05
+qqPlot(rstudent(m_monoc), pch=16)  #dentro do envelope 
+plot(predict(m_monoc), rstudent(m_monoc), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok?
+
+m_eosin <- lm(teps_total ~ eosinofilos, bd)
+summary(m_eosin) #R: 0,0828 p: 0,031
+shapiro.test(rstudent(m_eosin))    #-aceita p>0,05
+qqPlot(rstudent(m_eosin), pch=16)  #dentro do envelope 
+plot(predict(m_eosin), rstudent(m_eosin), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok?
+
+m_plaq <- lm(teps_total ~ plaquetas_vol, bd)
+summary(m_plaq) #R: 0,0081 p: 0,250
+shapiro.test(rstudent(m_plaq))    #-aceita p>0,05
+qqPlot(rstudent(m_plaq), pch=16)  #dentro do envelope 
+plot(predict(m_plaq), rstudent(m_plaq), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok
+
+m_anose <- lm(teps_total ~ anos_estudo, bd)
+summary(m_anose) #R: -0,0062 p: 0,381
+shapiro.test(rstudent(m_anose))    #-aceita p>0,05
+qqPlot(rstudent(m_anose), pch=16)  #dentro do envelope 
+plot(predict(m_anose), rstudent(m_anose), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok
+
+m_renda <- lm(teps_total ~ renda_familia, bd)
+summary(m_renda) #R: -0,0181 p: 0,595
+shapiro.test(rstudent(m_renda))    #-aceita p>0,05
+qqPlot(rstudent(m_renda), pch=16)  #dentro do envelope 
+plot(predict(m_renda), rstudent(m_renda), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok?
+
+m_exerct <- lm(teps_total ~ exercicio_tempo, bd)
+summary(m_exerct) #R: 0,0549 p: 0,0734
+shapiro.test(rstudent(m_exerct))    #-aceita p>0,05
+qqPlot(rstudent(m_exerct), pch=16)  #dentro do envelope 
+plot(predict(m_exerct), rstudent(m_exerct), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #super tendencia
+
+m_exercd <- lm(teps_total ~ exercicio_duracao, bd)
+summary(m_exercd) #R: -0,0226 p: 0,945
+shapiro.test(rstudent(m_exercd))    #-aceita p>0,05
+qqPlot(rstudent(m_exercd), pch=16)  #dois pontos saem do envelope
+plot(predict(m_exercd), rstudent(m_exercd), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #super tendencia
+
+m_intern <- lm(teps_total ~ internacao_quant, bd)
+summary(m_intern) #R: -0,0222 p: 0,887
+shapiro.test(rstudent(m_intern))    #-aceita p>0,05
+qqPlot(rstudent(m_intern), pch=16)  #dois pontos saem do envelope
+plot(predict(m_intern), rstudent(m_intern), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #super tendencia
+
+
+
+
+
+
+
+
+
+##############PROPOSTA 2: TEPS VARIAVEL DEPENDENTE#######################
+
+###INDEPENDENTES: outros questionarios
+
+
+prop1 <- lm(teps_total ~ bas_total + bis_total + panas_neg + panas_pos +
+              hads_total + hads_ans + hads_dep + neuroticismo +
+              quesi_abuso_emoc + quesi_abuso_fis + quesi_abuso_sex +
+              quesi_neglig_emoc + quesi_neglig_fis + quesi_total, bd)
+summary(prop1)#42,56
+na.omit(step(prop1))
+vif(prop1)
+##multic: hads_total, hads_ans, quesi_total
+step(prop1)
+
+problema1 <- lm(teps_total ~ hads_total + hads_ans + quesi_total, bd)
+summary(problema1)
+vif(problema1)
+
+##1 - sem o hads_dep e quesi_total
+prop1.1 <- lm(teps_total ~ bas_total + bis_total + panas_neg + panas_pos + 
+                hads_total + hads_ans + neuroticismo + quesi_abuso_emoc + 
+                quesi_abuso_fis + quesi_abuso_sex + quesi_neglig_emoc + quesi_neglig_fis, bd)
+summary(prop1.1)#0,4256
+na.omit(step(prop1.1))
+vif(prop1.1)##hads_total e hads_ans
+
+##tirando hads_total - multicolinearidade
+prop1.2 <- lm(teps_total ~ bas_total + bis_total + panas_neg + panas_pos + 
+                hads_ans + neuroticismo + quesi_abuso_emoc + 
+                quesi_abuso_fis + quesi_abuso_sex + quesi_neglig_emoc + quesi_neglig_fis, bd)
+summary(prop1.2)#0,278
+vif(prop1.2)
+
+##OU tirando hads_ans
+prop1.3 <- lm(teps_total ~ bas_total + bis_total + panas_neg + panas_pos + 
+                hads_total + neuroticismo + quesi_abuso_emoc + 
+                quesi_abuso_fis + quesi_abuso_sex + quesi_neglig_emoc + quesi_neglig_fis, bd)
+summary(prop1.3)#0,3916
+vif(prop1.3)
+AIC(prop1.2, prop1.3) #####1.3
+#teste de residuos
+shapiro.test(rstudent(prop1.3))    #-aceita p>0,05
+qqPlot(rstudent(prop1.3), pch=16)  #dentro do envelope
+plot(predict(prop1.3), rstudent(prop1.3), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok
+step(prop1.3)
+
+prop2 <- lm(teps_total ~ bas_total + bis_total + panas_neg + hads_total + 
+              neuroticismo + quesi_abuso_emoc + quesi_abuso_fis + quesi_abuso_sex + 
+              quesi_neglig_fis, bd)
+summary(prop2)#0,4232
+vif(prop2)
+AIC(prop2, prop1.3) #####2
+#teste de residuos
+shapiro.test(rstudent(prop2))    #-aceita p>0,05
+qqPlot(rstudent(prop2), pch=16)  #dentro do envelope
+plot(predict(prop2), rstudent(prop2), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ok
+step(prop2)
+
+##tirando abuso_sex
+prop2.2 <- lm(teps_total ~ bas_total + bis_total + panas_neg + hads_total + 
+              neuroticismo + quesi_abuso_emoc + quesi_abuso_fis + 
+              quesi_neglig_fis, bd)
+summary(prop2.2)#0,432
+vif(prop2.2)
+AIC(prop2, prop2.2) #####2.2
+#teste de residuos
+shapiro.test(rstudent(prop2.2))    #-aceita p>0,05
+qqPlot(rstudent(prop2.2), pch=16)  #dentro do envelope
+plot(predict(prop2.2), rstudent(prop2.2), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ponto na linha
+step(prop2.2)
+
+prop3 <- lm(teps_total ~ bas_total + hads_total + neuroticismo + quesi_abuso_emoc + 
+              quesi_abuso_fis + quesi_neglig_fis, bd)
+summary(prop3)#0,4505
+vif(prop3)
+AIC(prop3, prop2.2) #####3
+#teste de residuos
+shapiro.test(rstudent(prop3))    #-aceita p>0,05
+qqPlot(rstudent(prop3), pch=16)  #ponto fora do envelope
+plot(predict(prop3), rstudent(prop3), pch=19, 
+     ylim=c(-3,3)); abline(h=c(-3,0,3),lty=2) #ponto fora
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- vanessa_hu_biplot.r
+++ vanessa_hu_biplot.r
@@ -15,6 +15,36 @@ names(exames)
 exames$X <- NULL
 
 
+modelo1 <- data.frame(bd$teps_total, bd$idade, bd$imc, bd$cortisol, bd$CRH, bd$PCR,
+                        bd$hemacias, bd$hemoglobina, bd$hematocrito, bd$volumeglob,
+                      bd$hemoglobina_concent, bd$rdw, bd$leucocitos, bd$segmentados,
+                      bd$bastonados, bd$linfocitos, bd$monocitos, bd$eosinofilos, 
+                      bd$basofilos, bd$plaquetas, bd$plaquetas_vol, bd$tempo_casado,
+                      bd$num_filhos, bd$anos_estudo, bd$renda_familia, bd$quant_pessoas,
+                      bd$cigarros_semana, bd$tempo_tabagismo, bd$exercicio_tempo,
+                      bd$exercicio_duracao, bd$traumatismo_tempo, bd$peso, bd$altura,
+                      bd$internacao_quant, bd$internacao_geral_quant)
+
+modelo2 <- data.frame(bd$teps_total, bd$idade, bd$CRH, bd$PCR,
+                      bd$hemoglobina_concent, bd$leucocitos, bd$segmentados,
+                      bd$linfocitos, bd$monocitos, bd$eosinofilos, 
+                      bd$plaquetas_vol, bd$anos_estudo, bd$renda_familia, bd$exercicio_tempo,
+                      bd$exercicio_duracao, bd$internacao_quant)
+
+modelo4 <- data.frame(bd$teps_total, bd$hemoglobina_concent, bd$linfocitos,
+                      bd$anos_estudo, bd$exercicio_tempo, bd$exercicio_duracao)
+
+proposta2 <- data.frame(bd$teps_total, bd$bas_total, bd$bis_total, bd$panas_neg, bd$hads_total, 
+                        bd$neuroticismo, bd$quesi_abuso_emoc, bd$quesi_abuso_fis, bd$quesi_abuso_sex,
+                        bd$quesi_neglig_fis)
+
+perigo <- data.frame(bd$teps_total, bd$segmentados, bd$linfocitos, bd$monocitos,
+                    bd$eosinofilos)
+
+problema <- data.frame(bd$teps_total, bd$hads_total, bd$hads_ans, bd$quesi_total)
+
+?data.frame
+
 ###--- Todas as variaveis quanti
 w <- na.omit(quant); head(w)
 dim(w)
@@ -27,19 +57,48 @@ dim(v)
 round(sqrt(diag(cov(v))),4) # variancia
 round(cor(v),2)             # matriz de correlacao
 
-
 ###--- EXAMES
 x <- na.omit(exames); head(x)
 dim(x)
 round(sqrt(diag(cov(x))),4) # variancia
 round(cor(x),2)             # matriz de correlacao
 
+###--- MODELO1
+mod1 <- na.omit(modelo1); head(mod1)
+round(sqrt(diag(cov(mod1))),4) # variancia
+round(cor(mod1),2)             # matriz de correlacao
+
+###--- MODELO2
+mod2 <- na.omit(modelo2); head(mod2)
+round(sqrt(diag(cov(mod2))),4) # variancia
+round(cor(mod2),2)             # matriz de correlacao
+
+###--- MODELO2
+mod4 <- na.omit(modelo4); head(mod4)
+round(sqrt(diag(cov(mod4))),4) # variancia
+round(cor(mod4),2)             # matriz de correlacao
+
+###--- PROPOSTA2
+p2 <- na.omit(proposta2); head(p2)
+round(sqrt(diag(cov(p2))),4) # variancia
+round(cor(p2),2)             # matriz de correlacao
+
+###--- PERIGO
+pe <- na.omit(perigo); head(pe)
+round(sqrt(diag(cov(pe))),4) # variancia
+round(cor(pe),2)             # matriz de correlacao
+
+###--- PROBLEMA
+pr <- na.omit(problema); head(pr)
+round(sqrt(diag(cov(pr))),4) # variancia
+round(cor(pr),2)             # matriz de correlacao
+
 
 # grafico de correlacao
 library(corrplot)
 
 #TODOS AS QUANTI
-Mcor<- cor(w)
+Wcor<- cor(w)
 corrplot(Mcor, type="upper", method='ellipse', tl.pos="t1", tl.cex = 0.6)
 ?corrplot
 corrplot(Mcor, type="lower", method='number' , tl.pos="n", tl.cex = 0.6, cl.pos="n", number.cex=0.5, add=T)
@@ -55,6 +114,37 @@ Xcor<- cor(x)
 corrplot(Xcor, type="upper", method='ellipse', tl.pos="t1")
 corrplot(Xcor, type="lower", method='number' , tl.pos="n", cl.pos="n", number.cex=0.5, add=T)
 
+#MODELO1
+Mcor<- cor(m)
+corrplot(Mcor, type="upper", method='ellipse', tl.pos="t1")
+corrplot(Mcor, type="lower", method='number' , tl.pos="n", cl.pos="n", number.cex=0.5, add=T)
+
+#MODELO2
+M2cor<- cor(mod2)
+corrplot(M2cor, type="upper", method='ellipse', tl.pos="t1")
+corrplot(M2cor, type="lower", method='number' , tl.pos="n", cl.pos="n", number.cex=0.5, add=T)
+
+#MODELO2
+M4cor<- cor(mod4)
+corrplot(M4cor, type="upper", method='ellipse', tl.pos="t1")
+corrplot(M4cor, type="lower", method='number' , tl.pos="n", cl.pos="n", number.cex=0.5, add=T)
+
+#PROPOSTA2
+P2cor<- cor(p2)
+corrplot(P2cor, type="upper", method='ellipse', tl.pos="t1")
+corrplot(P2cor, type="lower", method='number' , tl.pos="n", cl.pos="n", number.cex=0.5, add=T)
+
+#PERIGO
+Pecor<- cor(pe)
+corrplot(Pecor, type="upper", method='ellipse', tl.pos="t1")
+corrplot(Pecor, type="lower", method='number' , tl.pos="n", cl.pos="n", number.cex=0.5, add=T)
+##
+
+#PROBLEMA
+Prcor<- cor(pr)
+corrplot(Prcor, type="upper", method='ellipse', tl.pos="t1")
+corrplot(Prcor, type="lower", method='number' , tl.pos="n", cl.pos="n", number.cex=0.5, add=T)
+##
 
 
 # Teste de correlacoes
@@ -191,6 +281,35 @@ acp_x<- prcomp(x)
 summary(acp_x)                   # Contribuicao de cada componente principal
 round(summary(acp_x)$rotation,2) # Contribuicao de cada variavel na CP
 
+#----- ACP  MODELO1
+acp_mod1<- prcomp(mod1)
+summary(acp_mod1)                   # Contribuicao de cada componente principal
+round(summary(acp_mod1)$rotation,2) # Contribuicao de cada variavel na CP
+
+#----- ACP  MODELO2
+acp_mod2<- prcomp(mod2)
+summary(acp_mod2)                   # Contribuicao de cada componente principal
+round(summary(acp_mod2)$rotation,2) # Contribuicao de cada variavel na CP
+
+#----- ACP  MODELO4
+acp_mod4<- prcomp(mod4)
+summary(acp_mod4)                   # Contribuicao de cada componente principal
+round(summary(acp_mod4)$rotation,2) # Contribuicao de cada variavel na CP
+
+#----- ACP  PROPOSTA2
+acp_p2<- prcomp(p2)
+summary(acp_p2)                   # Contribuicao de cada componente principal
+round(summary(acp_p2)$rotation,2) # Contribuicao de cada variavel na CP
+
+#----- ACP  PERIGO
+acp_pe<- prcomp(pe)
+summary(acp_pe)                   # Contribuicao de cada componente principal
+round(summary(acp_pe)$rotation,2) # Contribuicao de cada variavel na CP
+
+#----- ACP  PROBLEMA
+acp_pr<- prcomp(pr)
+summary(acp_pr)                   # Contribuicao de cada componente principal
+round(summary(acp_pr)$rotation,2) # Contribuicao de cada variavel na CP
 
 
 #---------------- BIPLOT
@@ -212,6 +331,36 @@ plot(v_hj, obj.names=F)
 x_hj<- bpca(x, method='hj'); names(x_hj); round(x_hj$eigenvalues,4)
 summary(x_hj)
 plot(x_hj, obj.names=F)
+
+####MODELO1
+m_hj<- bpca(mod1, method='hj'); names(m_hj); round(m_hj$eigenvalues,4)
+summary(m_hj)
+plot(m_hj, obj.names=F, xlim = c(-4,4), ylim = c(-4,4))
+
+####MODELO2
+m2_hj<- bpca(mod2, method='hj'); names(m2_hj); round(m2_hj$eigenvalues,4)
+summary(m2_hj)
+plot(m2_hj, obj.names=F, xlim = c(-4,4), ylim = c(-4,4))
+
+####MODELO4
+m4_hj<- bpca(mod4, method='hj'); names(m4_hj); round(m4_hj$eigenvalues,4)
+summary(m4_hj)
+plot(m4_hj, obj.names=F, xlim = c(-4,4), ylim = c(-4,4))
+
+####PROPOSTA2
+p2_hj<- bpca(p2, method='hj'); names(p2_hj); round(p2_hj$eigenvalues,4)
+summary(p2_hj)
+plot(p2_hj, obj.names=F)
+
+####PERIGO
+pe_hj<- bpca(pe, method='hj'); names(pe_hj); round(pe_hj$eigenvalues,4)
+summary(pe_hj)
+plot(pe_hj, obj.names=F)
+
+####PERIGO
+pr_hj<- bpca(pr, method='hj'); names(pr_hj); round(pr_hj$eigenvalues,4)
+summary(pr_hj)
+plot(pr_hj, obj.names=F)
 
 
 ### Biplot por grupos
